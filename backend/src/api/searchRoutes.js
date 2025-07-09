4b1d965cf65dd422fb9e51087efe7ae2dabaf1f9
icyhii <what.if.musiic@gmail.com>
2025-07-09 08:59:40 +0000

    chore: remove unused Express server files for Vercel serverless deployment

--- backend/src/api/searchRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { streamSearch } from '../controllers/searchController.js';
-
-const router = Router();
-
-router.get('/search', streamSearch);
-
-export default router;
